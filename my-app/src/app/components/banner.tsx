7850728671f36f3ebe5848869ba2638bea85ba9d
tolik4813 <afikusovich@gmail.com>
2024-07-25 00:37:42 +0300

    try to connect api

--- my-app/src/app/components/banner.tsx
+++ my-app/src/app/components/banner.tsx
@@ -1,4 +1,14 @@
-export default function Banner() {
+import { fetchNowPlaying } from "@/actions/actions"
+
+export default async function Banner() {
+    const banner = await fetchNowPlaying()
+
+    const banners = await fetchNowPlaying();
+
+    if (!banners.results) {
+      return <div>Loading...</div>;
+    }
+    
   return (
     <div>Banner</div>
   )
